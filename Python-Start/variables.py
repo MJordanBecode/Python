4c1c9dbe8ea43217833518a285a65e1efe6d26ca
Masy Jordan <masyjordan@gmail.com>
2024-07-30 14:57:34 +0200

    first ex

---
+++ Python-Start/variables.py
@@ -0,0 +1,8 @@
+name = "Alan Turing"
+age = 42
+person = [name, age, "mathematician"]
+
+typeage = type(age)
+print("Hello, my name is {} and I am {} years old and I am a {}".format(person[0], person[1], person[2]))
+print(typeage)
+
