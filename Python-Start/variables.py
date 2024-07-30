33a95d9e712826bc51f34829e10939a2bf95e4fe
Masy Jordan <masyjordan@gmail.com>
2024-07-30 22:32:42 +0200

    a

--- Python-Start/variables.py
+++ Python-Start/variables.py
@@ -1,8 +1,32 @@
 name = "Alan Turing"
-age = 42
+age = 17
 person = [name, age, "mathematician"]
 
 typeage = type(age)
 print("Hello, my name is {} and I am {} years old and I am a {}".format(person[0], person[1], person[2]))
 print(typeage)
 
+a  = 4
+b = 12
+
+floorDiv = b // a
+print(floorDiv)
+
+x = 2
+print(x == 2)
+print(x ==3)
+print(x < 3)
+
+if age >=18:
+    print("You're an adult!")
+elif age < 18:
+    print("You're younger than adulte")
+    
+    
+student1 = "John"
+student2 = "Eric"
+
+if student1 == "John" and student2 == "Eric":
+    print("You are John AND Eric")   
+else :
+    print("Welcome anonymous")
